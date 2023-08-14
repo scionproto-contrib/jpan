928f4df23c542533bdc9d674adc048e786cef150
Tilmann Zäschke <tilmann.zaeschke@inf.ethz.ch>
2023-08-14 16:16:05 +0200

    checkstyle

--- src/test/java/protobuf/SmokeTest.java
+++ src/test/java/protobuf/SmokeTest.java
@@ -22,13 +22,13 @@ import static org.junit.jupiter.api.Assertions.*;
 
 public class SmokeTest {
 
-    @Test
-    public void smoketest() {
-        Drkey.Protocol protocol = Drkey.Protocol.forNumber(1);
-        assertNotNull(protocol);
-        assertEquals(1, protocol.getNumber());
+  @Test
+  public void smoketest() {
+    Drkey.Protocol protocol = Drkey.Protocol.forNumber(1);
+    assertNotNull(protocol);
+    assertEquals(1, protocol.getNumber());
 
-        Daemon.Service daemonService = Daemon.Service.newBuilder().build();
-        assertNotNull(daemonService.getUri());
-    }
+    Daemon.Service daemonService = Daemon.Service.newBuilder().build();
+    assertNotNull(daemonService.getUri());
+  }
 }
