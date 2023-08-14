f7b8165d61923c9430881584465c38bb570311ef
Tilmann Zäschke <tilmann.zaeschke@inf.ethz.ch>
2023-08-14 14:54:44 +0200

    Restructuring

--- README.md
+++ README.md
@@ -1,3 +1,34 @@
 # SCION Java client
 
 A Java client for [SCION](https://scion.org).
+
+
+## FAQ / Trouble shooting
+
+### Cannot find symbol javax.annotation.Generated
+
+```
+Compilation failure: Compilation failure: 
+[ERROR] ...<...>ServiceGrpc.java:[7,18] cannot find symbol
+[ERROR]   symbol:   class Generated
+[ERROR]   location: package javax.annotation
+```
+
+This can be fixed by building with Java JDK 1.8.
+
+
+
+## Design consideration
+
+- Use Maven (instead of Gradle or something else): Maven is still the most used framework and arguable the best (convention over configuration)
+- Use Java 8: Still the most used JDK (TODO provide reference).
+  Main problem: no module support
+- Use slfj4 logging framework: widely used ond flexible.
+- Use Junit 5.
+- Use Google style guide for code
+
+
+### TODO
+- Where to place generated files? Currently in `target` but could be in `scr/java`...
+- 
+
---
+++ src/main/java/org/scion/DaemonClient.java
@@ -0,0 +1,113 @@
+package org.scion;// Copyright 2023 ETH Zurich
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//   http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+import io.grpc.*;
+import org.scion.proto.daemon.Daemon;
+import org.scion.proto.daemon.DaemonServiceGrpc;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import java.util.Collections;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.TimeUnit;
+
+public class DaemonClient {
+
+    private static final Logger logger = LoggerFactory.getLogger(DaemonClient.class.getName());
+
+    private final DaemonServiceGrpc.DaemonServiceBlockingStub blockingStub;
+    private final DaemonServiceGrpc.DaemonServiceStub asyncStub;
+    private final DaemonServiceGrpc.DaemonServiceFutureStub futureStub;
+
+    private Random random = new Random();
+
+    /**
+     * Construct client for accessing RouteGuide server using the existing channel.
+     */
+    public DaemonClient(Channel channel) {
+        blockingStub = DaemonServiceGrpc.newBlockingStub(channel);
+        asyncStub = DaemonServiceGrpc.newStub(channel);
+        futureStub = DaemonServiceGrpc.newFutureStub(channel);
+    }
+
+    /**
+     * Blocking unary call example.
+     */
+    public List<Daemon.Path> getPath(long srcIsdAs, long dstIsdAs) {
+        info("*** GetPath: src={0} lon={1}", srcIsdAs, dstIsdAs);
+
+        Daemon.PathsRequest request = Daemon.PathsRequest.newBuilder().setSourceIsdAs(srcIsdAs).setDestinationIsdAs(dstIsdAs).build();
+
+        Daemon.PathsResponse response;
+        try {
+            response = blockingStub.paths(request);
+        } catch (StatusRuntimeException e) {
+            warning("RPC failed: {0}", e.getStatus());
+            return Collections.emptyList();
+        }
+
+        return response.getPathsList();
+    }
+
+    /**
+     * Issues several different requests and then exits.
+     */
+    public static void main(String[] args) throws InterruptedException {
+        String target = "localhost:8980";
+        if (args.length > 0) {
+            if ("--help".equals(args[0])) {
+                System.err.println("Usage: [target]");
+                System.err.println("");
+                System.err.println("  target  The server to connect to. Defaults to " + target);
+                System.exit(1);
+            }
+            target = args[0];
+        }
+
+        // TODO credentials?
+        ManagedChannel channel = Grpc.newChannelBuilder(target, InsecureChannelCredentials.create())
+                .build();
+        List<Daemon.Path> paths;
+        try {
+            DaemonClient client = new DaemonClient(channel);
+            // Looking for a valid feature
+            client.getPath(409146138, -746188906);
+
+            // Feature missing.
+            paths = client.getPath(0, 0);
+
+            // Looking for features between 40, -75 and 42, -73.
+        } finally {
+            channel.shutdownNow().awaitTermination(5, TimeUnit.SECONDS);
+        }
+
+        System.out.println("Paths found: " + paths.size());
+        for (Daemon.Path path : paths) {
+            System.out.println("Path: first hop = " + path.getInterface().getAddress().getAddress());
+            int i = 0;
+            for (Daemon.PathInterface segment : path.getInterfacesList()) {
+                System.out.println("    " + i + ": " + segment.getId() + " " + segment.getIsdAs());
+            }
+        }
+    }
+
+    private void info(String msg, Object... params) {
+        logger.info(msg, params);
+    }
+
+    private void warning(String msg, Object... params) {
+        logger.warn(msg, params);
+    }
+}
