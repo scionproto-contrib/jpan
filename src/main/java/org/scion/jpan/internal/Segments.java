ca8c0bfcd4886bc8951882cc24411dbd4ff63dfb
Tilmann Zäschke <tilmann.zaeschke@inf.ethz.ch>
2024-05-17 17:44:55 +0200

    Path lookup errors

--- src/main/java/org/scion/jpan/internal/Segments.java
+++ src/main/java/org/scion/jpan/internal/Segments.java
@@ -177,11 +177,16 @@ public class Segments {
       }
       return getPathSegments(response);
     } catch (StatusRuntimeException e) {
-      if (e.getStatus().getCode().equals(Status.Code.UNKNOWN)
-          && e.getMessage().contains("TRC not found")) {
-        String msg = ScionUtil.toStringIA(srcIsdAs) + " / " + ScionUtil.toStringIA(dstIsdAs);
-        throw new ScionRuntimeException(
-            "Error while getting Segments: unknown src/dst ISD-AS: " + msg, e);
+      if (e.getStatus().getCode().equals(Status.Code.UNKNOWN)) {
+        if (e.getMessage().contains("TRC not found")) {
+          String msg = ScionUtil.toStringIA(srcIsdAs) + " / " + ScionUtil.toStringIA(dstIsdAs);
+          throw new ScionRuntimeException(
+              "Error while getting Segments: unknown src/dst ISD-AS: " + msg, e);
+        }
+        if (e.getMessage().contains("invalid request")) {
+          // path not found
+          return Collections.emptyList();
+        }
       }
       if (e.getStatus().getCode().equals(Status.Code.UNAVAILABLE)) {
         throw new ScionRuntimeException(
