b796e9e91d7af0a2e60dbdacb2a30bff2f24396f
Tilmann Zäschke <tilmann.zaeschke@inf.ethz.ch>
2024-06-28 18:46:07 +0200

    toStringIP

--- src/test/java/org/scion/jpan/demo/util/ToStringUtil.java
+++ src/test/java/org/scion/jpan/demo/util/ToStringUtil.java
@@ -17,6 +17,7 @@ package org.scion.jpan.demo.util;
 import java.net.Inet6Address;
 import java.net.InetAddress;
 import java.net.InetSocketAddress;
+import java.net.UnknownHostException;
 import java.nio.ByteBuffer;
 import org.scion.jpan.demo.inspector.HopField;
 import org.scion.jpan.demo.inspector.InfoField;
@@ -24,81 +25,12 @@ import org.scion.jpan.demo.inspector.PathHeaderScion;
 
 public class ToStringUtil {
 
-  public static String toStringIPv4(int ip) {
-    int mask = 0xFF000000;
-    String s = "";
-    s += ((ip & mask) >>> 24) + ".";
-    s += ((ip & (mask >>> 8)) >>> 16) + ".";
-    s += ((ip & (mask >>> 16)) >>> 8) + ".";
-    s += (ip & (mask >>> 24));
-    return s;
-  }
-
-  public static String toStringIPv6(int len, int... ips) {
-    String s = "";
-    for (int i = 0; i < len; i++) {
-      String s2 = Integer.toHexString(ips[i] >>> 16);
-      String s3 = Integer.toHexString(ips[i] & 0xFFFF);
-      s += s2 + ":" + s3;
-      if (i < len - 1) {
-        s += ":";
-      }
+  public static String toStringIP(byte[] b) {
+    try {
+      return InetAddress.getByAddress(b).getHostAddress();
+    } catch (UnknownHostException e) {
+      throw new IllegalArgumentException(e);
     }
-    // TODO not quite correct, we should replace the LONGEST sequence of 0:0 instead of the FIRST
-    // one.
-    int pos = s.indexOf("0:0:");
-    if (pos >= 0) {
-      int pos2 = pos + 4;
-      for (; pos2 + 1 < s.length(); pos2 += 2) {
-        if (s.charAt(pos2) != '0' || s.charAt(pos2 + 1) != ':') {
-          break;
-        }
-      }
-      s = s.substring(0, pos) + s.substring(pos2 - 1);
-      if (pos == 0) {
-        s = ":" + s;
-      }
-    }
-    return s;
-  }
-
-  public static String toStringIPv4(byte[] bytes) {
-    return bytes[0] + "." + bytes[1] + "." + bytes[2] + "." + bytes[3];
-  }
-
-  public static String toStringIPv6(byte[] b) {
-    // TODO use custom fix-length builder with loop
-    Integer[] ints = new Integer[8];
-    for (int i = 0; i < ints.length; i++) {
-      ints[i] = b[i * 2] * 256 + b[i * 2 + 1];
-    }
-    String s = String.format("%x:%x:%x:%x:%x:%x:%x:%x", (Object[]) ints);
-    //    String s = String.format("%x%02x:%x%02x:%x%02x:%x%02x:%x%02x:%x%02x:%x%02x:%x%02x",
-    //            b[0], b[1], b[2], b[3], b[4], b[5], b[6], b[7],
-    //            b[8], b[9], b[10], b[11], b[12], b[13], b[14], b[15]);
-    //    String s = Integer.toHexString(bytes[0]) + Integer.toHexString(bytes[1]) + ":" +
-    //                    Integer.toHexString(bytes[2]) + Integer.toHexString(bytes[3]) + ":" +
-    //                    Integer.toHexString(bytes[4]) + Integer.toHexString(bytes[5]) + ":" +
-    //                    Integer.toHexString(bytes[6]) + Integer.toHexString(bytes[7]) + ":" +
-    //                    Integer.toHexString(bytes[8]) + Integer.toHexString(bytes[9]) + ":" +
-    //                    Integer.toHexString(bytes[10]) + Integer.toHexString(bytes[11]) + ":" +
-    //                    Integer.toHexString(bytes[12]) + Integer.toHexString(bytes[13]) + ":" +
-    //                    Integer.toHexString(bytes[14]) + Integer.toHexString(bytes[15]);
-    // TODO not quite correct, we should replace the LONGEST sequence of 0:0 instead of the FIRST
-    // one.
-    //    int pos = s.indexOf(":0:");
-    //    if (pos >= 0) {
-    //      int pos2 = pos + 2;
-    //      while (pos2 + 2 < s.length() && s.charAt(pos2 + 1) == '0' && s.charAt(pos2 + 2) == ':')
-    // {
-    //        pos2 += 2;
-    //      }
-    //      s = s.substring(0, pos + 1) + s.substring(pos2);
-    //      if (s.startsWith("0:")) {
-    //        // TODO
-    //      }
-    //    }
-    return s;
   }
 
   /**
