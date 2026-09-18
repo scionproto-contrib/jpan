98e0777d6b24ba5ad2056957d5527ead9837dbb8
Tilmann Zäschke <tilmann.zaeschke@inf.ethz.ch>
2026-09-18 17:47:52 +0200

    ISD-AS list

--- src/main/java/org/scion/jpan/internal/bootstrap/LocalAsFromFile.java
+++ src/main/java/org/scion/jpan/internal/bootstrap/LocalAsFromFile.java
@@ -90,7 +90,7 @@ public class LocalAsFromFile {
     }
 
     return new LocalAS(
-        Collections.singleton(localIsdAs),
+        Collections.singletonList(localIsdAs),
         isCoreAs,
         localMtu,
         portRange,
